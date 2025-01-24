42745612eb930295044e0ba6b90147e972f8a3c0
Gleb <glebp@pm.me>
2025-01-24 16:46:54 +0100

    depth texture

--- src/render/state.rs
+++ src/render/state.rs
@@ -7,6 +7,7 @@ use crate::render::camera::{Camera, CameraController, CameraUniform};
 use crate::render::model::Vertex;
 use crate::render::texture;
 use cgmath::prelude::*;
+use crate::render::texture::Texture;
 
 const VERTICES: &[Vertex] = &[
     Vertex { position: [-0.0868241, 0.49240386, 0.0], tex_coords: [0.4131759, 0.00759614], }, // A
@@ -41,7 +42,7 @@ pub struct State<'a> {
     index_buffer: wgpu::Buffer,
     num_indices: u32,
     diffuse_bind_group: wgpu::BindGroup,
-    diffuse_texture: texture::Texture,
+    diffuse_texture: Texture,
 
     camera: Camera,
     camera_uniform: CameraUniform,
@@ -51,6 +52,8 @@ pub struct State<'a> {
 
     instances: Vec<Instance>,
     instance_buffer: wgpu::Buffer,
+
+    depth_texture: Texture,
 
     use_complex: bool,
 }
@@ -249,6 +252,9 @@ impl<'a> State<'a> {
                 push_constant_ranges: &[],
             });
 
+        let depth_texture = texture::Texture::create_depth_texture(&device, &config, "depth_texture");
+
+
         let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
             label: Some("Render Pipeline"),
             layout: Some(&render_pipeline_layout),
@@ -280,7 +286,13 @@ impl<'a> State<'a> {
                 // Requires Features::CONSERVATIVE_RASTERIZATION
                 conservative: false,
             },
-            depth_stencil: None, // 1.
+            depth_stencil: Some(wgpu::DepthStencilState {
+                format: texture::Texture::DEPTH_FORMAT,
+                depth_write_enabled: true,
+                depth_compare: wgpu::CompareFunction::Less, // 1.
+                stencil: wgpu::StencilState::default(), // 2.
+                bias: wgpu::DepthBiasState::default(),
+            }),
             multisample: wgpu::MultisampleState {
                 count: 1, // 2.
                 mask: !0, // 3.
@@ -318,6 +330,8 @@ impl<'a> State<'a> {
             }
         );
 
+
+
         Self {
             window,
             surface,
@@ -338,6 +352,7 @@ impl<'a> State<'a> {
             camera_controller,
             instances,
             instance_buffer,
+            depth_texture,
             use_complex: false
         }
     }
@@ -352,6 +367,8 @@ impl<'a> State<'a> {
             self.config.width = new_size.width;
             self.config.height = new_size.height;
             self.surface.configure(&self.device, &self.config);
+            self.depth_texture = Texture::create_depth_texture(&self.device, &self.config, "depth_texture");
+
         }
     }
 
@@ -406,7 +423,14 @@ impl<'a> State<'a> {
                         store: wgpu::StoreOp::Store,
                     },
                 })],
-                depth_stencil_attachment: None,
+                depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                    view: &self.depth_texture.view,
+                    depth_ops: Some(wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(1.0),
+                        store: wgpu::StoreOp::Store,
+                    }),
+                    stencil_ops: None,
+                }),
                 occlusion_query_set: None,
                 timestamp_writes: None,
             });
